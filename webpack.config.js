9aaf036cd780b0a6609b00ff898c0e3f83695349
advence-liz <1012515183@qq.com>
2018-07-25 22:53:45 +0800

    add: 初始结构

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/',
+  mode: 'development',
+  context: __dirname,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(art|ejs)$/,
+        loader: 'art-template-loader',
+        options: {
+          compileDebug: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'template/_layout.ejs',
+      favicon: 'template/favicon.ico',
+      // inject: false,
+      title: 'webpack'
+    })
+  ]
+}
