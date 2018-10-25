b6fe17e73a3a7b7d63db98f82179ba338eb47250
advence-liz <1012515183@qq.com>
2018-10-25 22:14:09 +0800

    del:util

--- alg/twoSum/index.js
+++ alg/twoSum/index.js
@@ -1,21 +1,4 @@
-console.log(module.i)
-
-const random = require('../../util/random')
-const { log, info } = console
-let arr = [
-  random(0, 10),
-  random(0, 10),
-  random(0, 10),
-  random(0, 10),
-  random(0, 10)
-]
-// console.log(arr)
-// Example:
-// Given nums = [2, 7, 11, 15], target = 9,
-//              [2, 7, 11, 15]
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
-
+const { log } = console
 function twoSum (arr = [], target) {
   log(arr, target)
   for (let i = 0, len = arr.length; i < len; i++) {
