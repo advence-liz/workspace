c578b536da5dfaa9f7b032a416beb32667ed94d6
advence-liz <1012515183@qq.com>
2019-04-17 18:59:47 +0800

    upd:

--- css/pc/app/index.js
+++ css/pc/app/index.js
@@ -2,9 +2,9 @@ var sun = new Image()
 var moon = new Image()
 var earth = new Image()
 function init () {
-  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png'
-  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png'
-  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png'
+  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png' // 300*300
+  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png' // 24*24
+  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png' // 7*7
   window.requestAnimationFrame(draw)
 }
 
@@ -26,26 +26,26 @@ function draw () {
       ((2 * Math.PI) / 60000) * time.getMilliseconds()
   )
   ctx.translate(105, 0)
-  ctx.fillRect(0, -12, 50, 24) // Shadow
+  // ctx.fillRect(0, -12, 50, 24) // Shadow
   ctx.drawImage(earth, -12, -12)
 
   // Moon
   ctx.save()
-  ctx.rotate(
-    ((2 * Math.PI) / 6) * time.getSeconds() +
-      ((2 * Math.PI) / 6000) * time.getMilliseconds()
-  )
-  ctx.translate(0, 28.5)
-  ctx.drawImage(moon, -3.5, -3.5)
+  // ctx.rotate(
+  //   ((2 * Math.PI) / 6) * time.getSeconds() +
+  //     ((2 * Math.PI) / 6000) * time.getMilliseconds()
+  // )
+  // ctx.translate(0, 28.5)
+  // ctx.drawImage(moon, -3.5, -3.5)
   ctx.restore()
-
+  // 为什么要运行两遍？因为save 了两遍
   ctx.restore()
 
   ctx.beginPath()
   ctx.arc(150, 150, 105, 0, Math.PI * 2, false) // Earth orbit
   ctx.stroke()
 
-  ctx.drawImage(sun, 0, 0, 300, 300)
+  // ctx.drawImage(sun, 0, 0, 300, 300)
 
   window.requestAnimationFrame(draw)
 }
