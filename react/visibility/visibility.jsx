b33bb699e3ebf7aa72e25a1a852b96674f8e2434
advence-liz <1012515183@qq.com>
2019-05-17 10:36:00 +0800

    upd:

--- react/visibility/visibility.jsx
+++ react/visibility/visibility.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  status = []
   state = {
-    show: false
+    status: []
   }
-  onChange = e => {}
+  componentDidMount () {
+    document.addEventListener('visibilitychange', () => {
+      const isHidden = document.hidden
+      const item = `document: ${document.visibilityState} ${new Date().toString()}`
+
+      this.status.push(item)
+      this.setState({ status: this.status })
+      document.title = isHidden ? '点回去' : 'visibility'
+      // Modify behavior...
+    })
+  }
   render () {
-    return <div>hello word</div>
+    const { status } = this.state
+    return (
+      <div>
+        <h1>visibility</h1>
+        {status.map((item, i) => (
+          <div key={i}>{item}</div>
+        ))}
+      </div>
+    )
   }
 }
