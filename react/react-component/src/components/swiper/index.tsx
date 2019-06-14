dee1f839cdb7acfb06f4b5a476942992ea9518fc
advence-liz <1012515183@qq.com>
2019-06-14 19:08:20 +0800

    upd:

--- react/react-component/src/components/swiper/index.tsx
+++ react/react-component/src/components/swiper/index.tsx
@@ -17,41 +17,87 @@ export interface ISwiperProps extends IBaseProps {}
  */
 @observer(['disabled'])
 export default class Swiper extends React.Component<ISwiperProps, any> {
-  static defaultProps = {
-    style: {}
-  }
+  static defaultProps = {}
 
-  state = { disabled: false, translateX: '' }
+  state = { originX: 0, endX: 0, index: 0, animation: {} }
   get classNames () {
     const { className } = this.props
     return classNames('q-swiper', className)
   }
-  translateX = () => {
-    const { translateX } = this.state
-    console.log(1)
-    this.setState({ translateX: '-100%' })
+  // translateX = () => {
+  //   const { translateX } = this.state
+  //   console.log(1)
+  //   this.setState({ translateX: '-100%' })
+  // }
+  animation (from = 0, to: number, duration = 300) {
+    let translateX = to - from
+    return {
+      transform: `translate(${translateX}px)`,
+      transition: `${duration}ms transform`
+    }
   }
+  onTouchStart = event => {
+    const { changedTouches: touches } = event
+    const touch = touches[0]
+    const { clientX, clientY } = touch
+    this.setState({ originX: clientX })
+  }
+  onTouchEnd = event => {
+    const { changedTouches: touches } = event
+    const touch = touches[0]
+    const { clientX: endX, clientY } = touch
 
+    let { originX, index } = this.state
+    if (endX - originX < -50) {
+      index++
+    }
+    if (endX - originX > 50) {
+      index--
+    }
+    
+    this.setState({ endX, index, animation: this.animation(0, -200 * index) })
+    if(index===6-1){
+      setTimeout(()=>{
+      this.setState({index:1,animation:this.animation(0,-200,0)})
+      },300)
+    }
+    // if(index===0){
+    //   setTimeout(()=>{
+    //   this.setState({index:6-1,animation:this.animation(0,-200*4,0)})
+    //   },300)
+    // }
+  }
+  onTouchMove = event => {
+    const { changedTouches: touches } = event
+    const touch = touches[0]
+    const { clientX, clientY } = touch
+    let { originX, index } = this.state
+    let translateX = clientX - originX - 200 * index
+
+    this.setState({ animation: this.animation(0, translateX, 0) })
+  }
   render () {
     const { style, children } = this.props
-    const { translateX } = this.state
+    const { animation, index } = this.state
     return (
       <div>
-        <button onClick={this.translateX}>translateX</button>
         <div className="q-swiper__wrap">
           <div
             className={this.classNames}
-            style={{
-              transform: `translate(${translateX})`,
-              transition: '0s transform'
-            }}
+            style={animation}
+            onTouchStart={this.onTouchStart}
+            onTouchEnd={this.onTouchEnd}
+            onTouchMove={this.onTouchMove}
           >
+            <div className="q-swiper__item">3</div>
+            <div className="q-swiper__item">0</div>
             <div className="q-swiper__item">1</div>
             <div className="q-swiper__item">2</div>
             <div className="q-swiper__item">3</div>
-            <div className="q-swiper__item">4</div>
+            <div className="q-swiper__item">0</div>
           </div>
         </div>
+        <h1>index:{index}</h1>
       </div>
     )
   }
