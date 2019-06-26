d53670c5fcb9a82073b679b0425fbddd2e0de1be
advence-liz <advence_liz@163.com>
2019-06-26 19:56:49 +0800

    upd:

--- doc/gremlins.md
+++ doc/gremlins.md
@@ -1,3 +1,34 @@
-# web端Monkey test库gremlins
+# web端 Monkeytest gremlins
 
-gremlins 是一个用JavaScript编写的web端Monkey test 库
+[gremlins](https://github.com/marmelab/gremlins.js) 是一个用JavaScript编写的强大的web端Monkeytest库
+
+## 背景
+
+最近要做web端`monkeytest`所以就对`monkeytest`方案进行漫无目的的检索最终发现基本就是两条路
+
+- 编写selenium-webdriver脚本自己用双手实现`monkeytest`
+- 使用`gremlins`
+  
+自己用`selenium-webdriver`实现`monkeytest`还是有点刺激的思考了5s先搁置，直接使用`gremlins`那么就需要更改网站现有逻辑也不是那么完美,但是把两种结合到一起看起来就很完美了即`selenium-webdriver`启动浏览器然后执行js脚本将`gremlins`动态的插入到站点并启动。
+
+这么酷炫的主意当然早就人想到了具体可以参考[automate-monkey-testing-with-selenium-webdriver-io-and-mocha](https://medium.com/@jlchereau/automate-monkey-testing-with-selenium-webdriver-io-and-mocha-337ea935e308)
+
+
+## 开门见山
+
+其最基本的用法如下洋洋洒洒一行代码就可以对当前站点执行`monkey test`
+
+```js
+<script src="path/to/gremlins.min.js"></script>
+<script>
+gremlins.createHorde().unleash();
+</script>
+```
+
+![](https://camo.githubusercontent.com/130e101ee69d4d9b6f065df0a0404c861eb5ce18/687474703a2f2f7374617469632e6d61726d656c61622e636f6d2f746f646f2e676966)
+
+## REF
+- [automate-monkey-testing-with-selenium-webdriver-io-and-mocha](https://medium.com/@jlchereau/automate-monkey-testing-with-selenium-webdriver-io-and-mocha-337ea935e308)
+- document.elementFromPoint
+- initEvent 不推荐使用了可以考虑fork 一份改
+- [chane seed](https://chancejs.com/usage/seed.html)
--- react/research/gremlins/index.jsx
+++ react/research/gremlins/index.jsx
@@ -20,22 +20,25 @@ function unleashGremlins (ttl = 100000, callback) {
        * @param {Long}    columnNumber   出错代码的列号
        * @param {Object}  errorObj       错误的详细信息，Anything
        */
-      window.onerror = function (
-        errorMessage,
-        scriptURI,
-        lineNumber,
-        columnNumber,
-        errorObj
-      ) {
-        console.info('错误信息：', errorMessage)
-        console.info('出错文件：', scriptURI)
-        console.info('出错行号：', lineNumber)
-        console.info('出错列号：', columnNumber)
-        console.info('错误详情：', errorObj)
-        window.monkeyErrors.count++
-        window.monkeyErrors.message.push({ errorMessage, scriptURI })
-      }
-      window.monkeyErrors = {
+      window.addEventListener(
+        'error',
+        (errorMessage, scriptURI, lineNumber, columnNumber, errorObj) => {
+          console.info('错误信息：', errorMessage)
+          console.info('出错文件：', scriptURI)
+          console.info('出错行号：', lineNumber)
+          console.info('出错列号：', columnNumber)
+          console.info('错误详情：', errorObj)
+          window.monkeyResult.count++
+          window.monkeyResult.message.push({
+            errorMessage,
+            scriptURI,
+            lineNumber,
+            columnNumber,
+            errorObj
+          })
+        }
+      )
+      window.monkeyResult = {
         count: 0,
         message: [],
         done: false
@@ -44,9 +47,19 @@ function unleashGremlins (ttl = 100000, callback) {
     })
     .after(function stopProfiler () {
       console.timeEnd('gremlins')
-      window.monkeyErrors.done = true
-      console.error('monkeyErrors', window.monkeyErrors)
+      window.monkeyResult.done = true
+      console.error('monkeyResult', window.monkeyResult)
     })
+    .gremlin(
+      window.gremlins.species.clicker().canClick(function (element) {
+        return element.className !== 'vc-switch'
+      })
+    )
+    .gremlin(window.gremlins.species.formFiller())
+    .gremlin(window.gremlins.species.toucher())
+    .gremlin(window.gremlins.species.scroller())
+    .gremlin(window.gremlins.species.typer())
+
     .mogwai(window.gremlins.mogwais.alert()) // 阻止了alert 弹出
     .mogwai(window.gremlins.mogwais.gizmo().maxErrors(2))
 
@@ -58,10 +71,21 @@ function unleashGremlins (ttl = 100000, callback) {
 }
 
 window.startMonkeyTest = function () {
-  var head = document.getElementsByTagName('head')[0]
+  const gremlinsSource =
+    'https://cdn.bootcss.com/gremlins.js/0.1.0/gremlins.min.js'
+  const head = document.getElementsByTagName('head')[0]
+  const scripts = document.getElementsByTagName('script')
+  let isFound = false
+  for (var i = 0; i < scripts.length; i++) {
+    if (scripts[i].src === gremlinsSource) {
+      isFound = true
+      break
+    }
+  }
+  if (isFound) return unleashGremlins()
 
-  var script = document.createElement('script')
-  script.src = 'https://cdn.bootcss.com/gremlins.js/0.1.0/gremlins.min.js'
+  const script = document.createElement('script')
+  script.src = gremlinsSource
   script.onload = () => {
     unleashGremlins()
   }
