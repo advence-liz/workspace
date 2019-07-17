c89ae163e24a3bdf78e8b7b38530ae69ea922af3
advence-liz <advence_liz@163.com>
2019-07-17 23:10:21 +0800

    upd: add pipeline

---
+++ jsl/functional/pipeline.js
@@ -0,0 +1,19 @@
+// ts-check
+var data = ['a', 'b', 'c']
+
+function adda (x) {
+  return x + 'a'
+}
+
+function addb (x) {
+  return x + 'b'
+}
+
+function pipeline (fns = [], data) {
+  return fns.reduce((x, y) => {
+    return x.map(y)
+  }, data)
+}
+
+const result = pipeline([adda, addb], data)
+console.dir(result)
