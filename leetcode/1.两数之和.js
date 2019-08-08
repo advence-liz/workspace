cac5427510bde358eb6bbae731e239d841839543
advence-liz <advence_liz@163.com>
2019-08-08 22:15:46 +0800

    upd: add twosun

---
+++ leetcode/1.两数之和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  var map = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentValue = nums[i]
+    const currentTarget = target - currentValue
+
+    if (map.has(currentTarget)) {
+      return [map.get(currentTarget), i]
+    }
+    map.set(currentValue, i)
+  }
+}
+// twoSum([2, 7, 11, 15], 9)
