d70855a0b2ea327cb3541e1d8d838ecb2c350497
advence-liz <advence_liz@163.com>
2019-08-11 23:58:04 +0800

    upd:

--- leetcode/24.两两交换链表中的节点.js
+++ leetcode/24.两两交换链表中的节点.js
@@ -16,18 +16,38 @@
  * pre->a->b
  *  pre ->b->a
  */
+// var swapPairs = function (head) {
+//   var ln = new ListNode(-1)
+//   ln.next = head
+//   var pre = ln
+//   while (pre.next && pre.next.next) {
+//     var a = pre.next
+//     var b = pre.next.next
+
+//     a.next = b.next
+//     b.next = a
+//     pre.next = b
+//     pre = pre.next.next
+//   }
+//   return ln.next
+// }
+
 var swapPairs = function (head) {
   var ln = new ListNode(-1)
-  ln.next = head
-  var pre = ln
-  while (pre.next && pre.next.next) {
-    var a = pre.next
-    var b = pre.next.next
+  var pre = new ListNode(-1)
+  ln.next = pre
+  pre.next = head
+
+  var cur = pre.next
+  while (cur && cur.next) {
+    var a = cur
+    var b = cur.next
 
     a.next = b.next
     b.next = a
     pre.next = b
-    pre = pre.next.next
+    pre = a
+    cur = cur.next.next
   }
-  return ln.next
+  return ln.next.next
 }
