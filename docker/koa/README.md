15718c0ec3fed8caf16598af9e81bfa868555bde
advence-liz <advence_liz@163.com>
2019-09-18 23:42:45 +0800

    upd: docker

--- docker/koa/README.md
+++ docker/koa/README.md
@@ -1,3 +1,5 @@
+- [node应用程序docker化](https://nodejs.org/zh-cn/docs/guides/nodejs-docker-webapp/)
+
 docker container run --rm -p 8000:3000 -it koa-demo  /bin/bash
 docker image build -t koa-demo .        
- docker container ls  --all   
+docker container ls  --all   
--- docker/koa/app.js
+++ docker/koa/app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 const app = new Koa()
+// Constants
+const PORT = 3000
+const HOST = '0.0.0.0'
 
 app.use(async ctx => {
-  ctx.body = 'Hello World1'
+  ctx.body = 'Hello World12'
 })
 
-app.listen(3000)
+console.log(PORT, HOST)
+app.listen(PORT)
