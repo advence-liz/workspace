94efaeae6b3d0fef86bb75ea3f4979fe85a36bed
advence-liz <advence_liz@163.com>
2019-10-28 23:46:03 +0800

    upd:

---
+++ alg/递归/fibonacci.js
@@ -0,0 +1,25 @@
+function* fibonacci() {
+  yield 1
+  yield 1
+  let [prev, curr] = [1, 1]
+  while (true) {
+    //                    n-2    n-1
+    [prev, curr] = [curr, prev + curr]
+    yield curr
+  }
+}
+
+function* fibonacci1(){
+  let current = 0
+  let next = 1
+  yield current
+  yield next
+
+  while(true) {
+    [current, next] = [next, current + next]
+    yield next
+  }
+}
+
+// https://mrxf.github.io/2017/09/21/my-view-of-fibonacci/
+// https://blog.csdn.net/qq_39300332/article/details/80000837
--- alg/递归/n!.js
+++ alg/递归/n!.js
@@ -1,8 +1,15 @@
-function factorial (n) {
+function factorial (n,level=0) {
+  // if(level>=5) {
+  //   console.log('overflow',level)
+  //   return
+  // }
+  console.log(level)
   if (n <= 0) return -1
   if (n === 1) return 1
 
-  return n * factorial(n - 1)
+  return n * factorial(n - 1,level+1)
 }
 let a = factorial(5)
-let b = factorial(6)
+let b = factorial(7)
+
+
