9f9f337a009b9f92dcb0776e2e0d7f8a1e89346f
advence-liz <advence_liz@163.com>
2019-11-08 17:20:21 +0800

    upd: promise

--- jsl/ECMASCRIPT/promise/index.js
+++ jsl/ECMASCRIPT/promise/index.js
@@ -1,27 +1,65 @@
 console.log(module.i)
-console.log(1)
-try {
-  new Promise(function(resolve, reject) {
-    console.log(2)
-    // resolve()
-    throw new Error('ee')
-    //                   reject('aaaa')
-  }).then(function() {
-    throw new Error('then err')
-  }).then
 
-//   .catch(err=>{
-//     console.log(err)
-//   })
-} catch (err) {
-  console.log('catch', err)
-}
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    throw new Error('throw error')
+  }, 0)
+}).catch(err => {
+  console.log(err, '捕获到异步错误')
+})
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('throw error')
+  }, 0)
+}).catch(err => {
+  console.log(err, '捕获到异步错误')
+})
+
+
+
+
+console.log(1)
+new Promise(function(resolve, reject) {
+  console.log(2)
+  resolve()
+}).then(function() {
+  console.log(4)
+}).then(function() {
+  console.log(5)
+})
 console.log(3)
 
-// p.catch(err=>{
-//     console.log('promise catch',err)
-// })
 
-// p.catch(err=>{
-//     console.log(2)
-// })
+
+
+new Promise((resolve, reject) => {
+  throw new Error('throw error')
+}).catch(err => {
+  console.log(err, '捕获到异步错误')
+})
+
+new Promise((resolve, reject) => {
+  reject('throw error')
+}).catch(err => {
+  console.log(err, '捕获到异步错误')
+})
+
+
+
+try {
+  console.log(1)
+  new Promise(function(resolve, reject) {
+    console.log(2)
+    throw new Error('?宏')
+  }).then(function() {
+    console.log(4)
+  })
+} catch (error) {
+  console.log('catch', error)
+}
+
+
+// 宏捕宏
+// 微捕微
+// 微捕宏
