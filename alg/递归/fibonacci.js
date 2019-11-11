6daa126f371c9bae2f890fb7c822014ac0481498
advence-liz <advence_liz@163.com>
2019-11-11 23:46:05 +0800

    upd:

--- alg/递归/fibonacci.js
+++ alg/递归/fibonacci.js
@@ -1,30 +1,60 @@
+// function* fibonacci() {
+//   yield 0
+//   yield 1
+//   yield 1
+//   let [prev, curr] = [1, 1]
+//   while (true) {
+//     //                    n-2    n-1
+//     [prev, curr] = [curr, prev + curr]
+//     yield curr
+//   }
+// }
+
+// function fibonacci2(n){
+//   if(n === 1 || n === 0 ) return n
+//   return fibonacci(n-1) + fibonacci(n-2)
+// }
+
+
+/**
+ *        | 0 , n = 0
+ * F(n) = | 1 , n = 1
+ *        | F(n-1)+F(n-2), n > 1
+ */
+
 function* fibonacci() {
-  yield 0
-  yield 1
-  yield 1
-  let [prev, curr] = [1, 1]
+  let t0 = 0
+  let t1 = 1
+  yield t0
+  yield t1
+
   while (true) {
-    //                    n-2    n-1
-    [prev, curr] = [curr, prev + curr]
-    yield curr
+    [t0, t1] = [t1, t0 + t1]
+    yield t1
   }
 }
 
-function fibonacci2(n){
-  if(n === 1 || n === 0 ) return n
-  return fibonacci(n-1) + fibonacci(n-2)
-}
-function* fibonacci1(){
-  let current = 0
-  let next = 1
-  yield current
-  yield next
-
-  while(true) {
-    [current, next] = [next, current + next]
-    yield next
+function getFibonacci(n) {
+  var gen = fibonacci()
+  var ret = -1
+  for (var i = 0; i <= n; i++) {
+    ret = gen.next().value
   }
+
+  return ret
 }
+
+var test = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+var isPass =test.every((value, index )=> {
+  var f = getFibonacci(index) 
+  console.log(f,value)
+  return f===value
+  
+})
+
+console.log('isPass',isPass)
+
 
 // https://mrxf.github.io/2017/09/21/my-view-of-fibonacci/
 // https://blog.csdn.net/qq_39300332/article/details/80000837
