4c145454f58c061155d157ee218731f79f24193f
advence-liz <advence_liz@163.com>
2019-11-24 18:06:19 +0800

    upd:

--- alg/递归/rlist.js
+++ alg/递归/rlist.js
@@ -12,22 +12,35 @@
  * 输入: 1->2->3->4->5->NULL
  * 输出: 5->4->3->2->1->NULL
  */
-var reverseList = function (head) {
-
-  var prev = null //前指针节点
-  var cur = head //当前指针节点
-  //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
-
-
-  while (cur) {
+var reverseList = function(head) {
+  var prev = null  
+  var cur = head
+      
+  // 1. 保留 next 节点
+  // 2. cur 指向 prev
+  // 3. prev cur 向后移动
+  while(cur){
     var next = cur.next
     cur.next = prev
+    
     prev = cur
     cur = next
-
-
-
+    
   }
   return prev
-
 }
+
+// var reverseList = function(head) {
+    
+//     var cur = head
+// 临界条件本身就有问题
+//     while(cur.next&&cur.next.next){
+//       var next = cur.next
+//       cur.next = next.next
+//       next.next = cur
+//       cur = cur.next
+  
+//     }
+    
+//     return cur
+//   }
