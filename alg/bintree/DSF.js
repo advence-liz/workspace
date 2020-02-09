dd252c6e2080e011d1163859730022bb52b086fb
advence-liz <advence_liz@163.com>
2020-02-09 22:27:09 +0800

    upd: 20-2-9

---
+++ alg/bintree/DSF.js
@@ -0,0 +1,50 @@
+/**
+ * 应用stack queue 解决深度和广度优先的问题可以思考普通的 tree 即有多个子节点的
+ * 深度优先 等于前序遍历
+ *
+ * @param {*} root
+ */
+function dsf(root) {
+  if (!root) return []
+  var stack = []
+  var results = []
+
+  stack.push(root)
+
+  while (stack.length) {
+    var cur = stack.pop()
+    cur && results.push(cur)
+
+    // 对于普通的tree 就是 stack.push(...children)
+
+    if (cur.right) {
+      stack.push(cur.right)
+    }
+    if (cur.left) {
+      stack.push(cur.left)
+    }
+  }
+}
+/**
+ * 节点一层一层进入队列，先进先出
+ * @param {*} root
+ */
+function bfs(root) {
+  var queue = []
+  var results = []
+  queue.push(root)
+
+  while (queue.length) {
+    var cur = queue.shift()
+
+    cur && results.push(cur)
+
+    if (cur.left) {
+      queue.push(cur.left)
+    }
+    if (cur.right) {
+      queue.push(cur.right)
+    }
+  }
+  return results
+}
--- leetcode/144.二叉树的前序遍历.js
+++ leetcode/144.二叉树的前序遍历.js
@@ -16,28 +16,60 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// var preorderTraversal = function (root) {
+//   if (root == null) {
+//     return []
+//   }
+//   var stack = []
+//   var result = []
+//   stack.push(root)
+//   while (stack.length) {
+//     var cur = stack.pop()
+//     cur && result.push(cur.val)
+
+//     if (cur.right) {
+//       stack.push(cur.right)
+//     }
+
+//     if (cur.left) {
+//       stack.push(cur.left)
+//     }
+
+//   }
+//   return result
+
+// }
+/**
+ * 广度优先（名字不能换了leetcode 就不行了）
+ * 广度优先就是 输入跟输出一样因为leetcode 的输入形式就是广度优先
+ * 应用stack queue 解决深度和广度优先的问题可以思考普通的 tree 即有多个子节点的
+ * @param {*} root 
+ */
 var preorderTraversal = function (root) {
   if (root == null) {
     return []
   }
-  var stack = []
+  var queue = []
   var result = []
-  stack.push(root)
-  while (stack.length) {
-    var cur = stack.pop()
+  queue.push(root)
+  while (queue.length) {
+    var cur = queue.shift()
     cur && result.push(cur.val)
+    // queue.push(...children)
+    if (cur.left) {
+      queue.push(cur.left)
+    }
 
     if (cur.right) {
-      stack.push(cur.right)
+      queue.push(cur.right)
     }
 
-    if (cur.left) {
-      stack.push(cur.left)
-    }
-
+    
   }
   return result
 
 }
+
+// [1,2,5,3,4]
 // @lc code=end
 
