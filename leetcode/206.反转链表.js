ba39958277265c9dca5b63c7a01ce99115fe8980
advence-liz <advence_liz@163.com>
2020-02-12 11:08:24 +0800

    upd: 2 12

--- leetcode/206.反转链表.js
+++ leetcode/206.反转链表.js
@@ -2,6 +2,8 @@
  * @lc app=leetcode.cn id=206 lang=javascript
  *
  * [206] 反转链表
+ * @ref
+ * https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
  */
 /**
  * Definition for singly-linked list.
@@ -9,22 +11,55 @@
  *     this.val = val;
  *     this.next = null;
  * }
+ *  输入: 1->2->3->4->5->NULL
+ *  输出: 5->4->3->2->1->NULL
  */
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {
-  var pre = null
+// var reverseList = function (head) {
+//   if(!head) return null
+//   var cur = head
+//   var pre = null
+//   while(cur){
+//     var next = cur.next
+//     cur.next = pre
+//     pre = cur
+//     cur = next 
 
-  var cur = head
+//   }
+//   return pre
 
-  while (cur) {
-    // var [cur.next,cur,pre]=[pre,cur.next,cur]
-    var next = cur.next
-    cur.next = pre
-    pre = cur
-    cur = next
+// }
+
+// var reverseList = function (head) {
+
+
+//   function reverse(pre, cur) {
+//     if (!cur) return pre
+//     var next = cur.next
+//     cur.next = pre
+    
+//     reverse(cur, next)
+
+//   }
+
+//   return reverse(null, head)
+
+
+// }
+
+let reverseList = (head) =>{
+  if (head.next == null) {
+    return head
   }
-  return pre
+
+  var last = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+
+  return last
+
 }
+
