ffada58b2d7d16838ce4faaca5f33b267f22f35a
advence-liz <advence_liz@163.com>
2020-02-19 21:33:14 +0800

    upd:

--- alg/jsl/curry.js
+++ alg/jsl/curry.js
@@ -13,13 +13,34 @@
 /* ================ 到这里结束 ================ */
 // https://www.zhangxinxu.com/wordpress/2013/02/js-currying/
 
-function add (x, y) {
-  if (x && y) {
-    return x + y
-  }
-  if (!y) {
-    return add.bind(null, x)
-  }
+// const add = x=>y => x+y
+
+// console.log(add(1)(2))
+
+// function add(x,y){
+//   return x+y
+// }
+// function curry(fn,...args){
+
+//   return function (...rest){
+//     return fn(...args,...rest)
+//   }
+// }
+
+// var r=curry(add,1)(2)
+// console.log(r)
+
+function add(x, y, z) {
+  return x + y + z
 }
 
-function curry (fn) {}
+function curry(fn, ...args) {
+  if (args.length >= fn.length) {
+    return fn(...args)
+  }
+  return function(...rest) {
+    return curry(fn, ...args, ...rest)
+  }
+}
+var r = curry(add, 1)(2)(3)
+console.log(r)
