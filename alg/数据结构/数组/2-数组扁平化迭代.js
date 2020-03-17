b7ce391bbbcbd8573ba1dce6b6ae8f7c02398e1c
advence-liz <advence_liz@163.com>
2020-03-17 10:15:38 +0800

    upd:

---
+++ alg/数据结构/数组/2-数组扁平化迭代.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, [3, 4, 5, [6, 7], 8], 9, 10, [11, [12, 13]]]
+
+const flatten = function (arr) {
+    while (arr.some(item => Array.isArray(item))) {
+        arr = [].concat(...arr)
+    }
+    return arr
+}
+
+console.log(flatten(arr))
