2d2011d5a15e87e9790fd63bc6e18ae1cc5bd9dd
advence-liz <advence_liz@163.com>
2020-04-07 16:56:39 +0800

    upd:

--- alg/数据结构/链表/helper.js
+++ alg/数据结构/链表/helper.js
@@ -2,29 +2,51 @@ function ListNode(val) {
     this.val = val
     this.next = null
 }
-
+/**
+ *
+ * @param {*} arr 链表源数组
+ * @description
+ * createLinkList([1, 2, 3, 4, 5])
+ * 1->2->3->4->5
+ * createLinkList([1, 2, 3, 4, 5, 3]) 形成环
+ * 1->2->3->4->5->3->4->5->3->4->5->3->4->5->3...
+ */
 function createLinkList(arr = [1, 2, 3, 4, 5]) {
     var dummyHead = new ListNode(-1)
     var cur = dummyHead
-
+    var m = new Map()
+    var pos = -1
+    var len = 0
     arr.forEach(val => {
-        cur.next = new ListNode(val)
+        if (m.has(val)) {
+            pos = arr.indexOf(val)
+            len = arr.length - pos-1
+            cur.next = m.get(val)
+        } else {
+            m.set(val, new ListNode(val))
+            cur.next = m.get(val)
+        }
         cur = cur.next
     })
+    let max = arr.length + len
+    print(dummyHead.next,max)
     return dummyHead.next
 }
 
-function print(head) {
+function print(head,max) {
     var res = []
     var cur = head
-    while (cur) {
+    var size = 0
+    while (cur && size < max) {
         res.push(cur.val)
         cur = cur.next
+        size++
     }
-    console.log(res)
+    if (size === max) res.push('...')
+    console.log(res.join('->'))
 }
-module.exports={
+module.exports = {
     createLinkList,
     print,
-    ListNode,
+    ListNode
 }
---
+++ alg/数据结构/链表/helper.test.js
@@ -0,0 +1,4 @@
+const { createLinkList } = require('./helper')
+
+createLinkList([1, 2, 3, 4, 5])
+createLinkList([1, 2, 3, 4, 5, 3])
