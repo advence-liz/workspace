50521f15ac7673048ed61c0d3f4e35315ee5e6ca
advence-liz <advence_liz@163.com>
2020-04-23 23:07:28 +0800

    upd: vue

---
+++ vue/sample/src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="btn btn--default" @click="$emit('click')">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    // // name: 'button',
+    // methods: {
+    //     reserver() {
+    //       this.$emit('reserver')
+    //     }
+    // },
+    // props: {
+    //     click
+    // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.btn {
+    display: inline-block;
+    &--default {
+        background: #ffffff;
+        border: 1px solid gray;
+    }
+}
+</style>
