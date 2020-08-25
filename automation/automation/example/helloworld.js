3debbfddc22e213b448ec85696543e4d9b331ad4
lizhuo13 <lizhuo13@meituan.com>
2020-08-25 19:57:23 +0800

    feat: 补充演示

--- automation/automation/example/helloworld.js
+++ automation/automation/example/helloworld.js
@@ -10,6 +10,8 @@ async function run () {
         .build()
     
     try {
+        // await driver.wait(until.urlContains('done'),10000)
+
         // 2 操作浏览器器打开指定页面
         await driver.get('https://www.baidu.com/')
         // 3 获取页面元素，并进行输入操作
@@ -17,10 +19,11 @@ async function run () {
         await driver.findElement(By.id('kw')).sendKeys('helloworld', Key.RETURN)
         // 4 等待页面title转变为指定值
         await driver.wait(until.titleIs('helloworld_百度搜索'), 1500)
+        await driver.wait(until.urlContains('done'),10000)
         return 'done'
     } finally {
         // 5 退出浏览器
-        await driver.quit()
+        // await driver.quit()
     }
 
     
