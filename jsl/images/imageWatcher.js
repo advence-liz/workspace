d72dd9d1b79e0dade6b7d197d43ae760af7a359f
lizhuo13 <lizhuo13@meituan.com>
2020-11-06 15:47:25 +0800

    add 添加图片检测脚本

--- jsl/images/imageWatcher.js
+++ jsl/images/imageWatcher.js
@@ -1,40 +1,46 @@
 /**
  *
  * 图片原始宽高 naturalWidth naturalHeight
- * 设计宽高 clientWidth clientHeight
+ * 设计宽高 width height   (clientWidth clientHeight 原始隐藏时值为0）
  *
  * 1. 监控是否使用超过6百万像素 宽 * 高 大于 6000000
  * 2. 图片原始宽高不得超过设计宽高
  */
 
-window.onload = function() {
-    var RootFontSize = +getComputedStyle(window.document.documentElement)['font-size'].match(/[^px]+/)[0]
-    var RootRatio = RootFontSize / 100
-    // RootRatio = RootRatio.toFixed(2)
+var DEBUG = true
 
+function imagesWatcher() {
+    if (window.screen.width !== 375) return
+    // if (parseInt(Math.random() * 10, 10) !== 3) return
     var MaxPixel = 6000000
-
     var Store = []
 
-    function ImageError(image, type) {
+    function ImageError(image, type, msg) {
         this.type = type
         this.src = image.src
         this.report = function() {
+            var tags = {
+                type,
+                src: this.src,
+                naturalWidth: image.naturalWidth,
+                naturalHeight: image.naturalHeight,
+                width: image.width,
+                height: image.height,
+                widthOffset: image.naturalWidth - image.width,
+                heightOffset: image.naturalHeight - image.height
+            }
+            if (DEBUG) {
+                console.table(tags)
+            }
             window.Owl &&
                 window.Owl.addError(
                     {
                         name: 'cubeImageLimitError',
-                        msg: '图片不符合限定条件'
+                        msg
                     },
                     {
                         level: 'error', // error、warn、info、debug 快速根据异常等级搜索上报日志
-                        tags: {
-                            type,
-                            src: this.src,
-                            RootRatio,
-                            RootFontSize,
-                            screenWidth: window.screen.width
-                        }
+                        tags
                     }
                 )
         }
@@ -43,7 +49,7 @@ window.onload = function() {
     function filterMaxPixel(image) {
         var inValid = image.naturalWidth * image.naturalHeight >= MaxPixel
         if (inValid) {
-            Store.push(new ImageError(image, 'MaxPixel'))
+            Store.push(new ImageError(image, 'MaxPixel', '图片超过6百万像素'))
         }
     }
     /**
@@ -51,13 +57,18 @@ window.onload = function() {
      * 图片原始宽度 / 图片设计宽度 得到的是精准值 比如： 828/750 1.104
      */
     function filterSizeMatch(image) {
-        var widthRatio = image.naturalWidth / image.clientWidth
-        var heightRatio = image.naturalHeight / image.clientHeight
-        // widthRatio = widthRatio.toFixed(2)
-        // heightRatio = heightRatio.toFixed(2)
-        var inValid = widthRatio > RootRatio || heightRatio > RootRatio
+        /**
+         * 原始算法为了兼容大屏小屏乘以 RootRatio 但是都会有问题。 image.naturalWidth * RootRatio > image.width || image.naturalHeight * RootRatio > image.height
+         * 1. 针对小屏计算会有一些的误差添加一个容错偏移量可以解决。比如根据图片实际尺寸 * RootRatio 计算出来的值是 51.555 而浏览器的自适应的宽度为 52 会自动取整
+         * 2. 针对大屏有些图片样式写死的像素值而不是rem 这样式计算乘以 RootRatio 会导致错误统计。
+         * 3. 没必要统计大屏和小屏，跳过就好。
+         */
+        var offset = 1 // 检测留一点偏移buffer
+        var widthOffset = image.naturalWidth - image.width
+        var heightOffset = image.naturalHeight - image.height
+        var inValid = widthOffset > offset || heightOffset > offset
         if (inValid) {
-            Store.push(new ImageError(image, 'SizeMatch'))
+            Store.push(new ImageError(image, 'SizeNotMatch', '图片原始尺寸与设计尺寸不匹配'))
         }
     }
 
@@ -65,7 +76,21 @@ window.onload = function() {
 
     for (var i = 0; i < images.length; i++) {
         var image = images[i]
+        if (DEBUG) {
+            console.log(image.src)
+            console.log('image natural', image.naturalWidth, image.naturalHeight)
+            console.log('image width', image.width, image.height)
+        }
+
         filterMaxPixel(image)
         filterSizeMatch(image)
     }
+
+    Store.forEach((item) => item.report())
+}
+
+if (document.readyState === 'complete') {
+    imagesWatcher()
+} else {
+    window.onload = imagesWatcher
 }
