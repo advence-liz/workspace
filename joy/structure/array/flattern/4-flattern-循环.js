638d61aaf030c2e37a74e0eda260d9bb2961e00e
advence-liz <advence_liz@163.com>
2020-11-18 00:18:09 +0800

    upd: flatten

--- joy/structure/array/flattern/4-flattern-循环.js
+++ joy/structure/array/flattern/4-flattern-循环.js
@@ -11,22 +11,20 @@ var list = [1, [2, 3, [4, 5]], [1, 2, 3, [4, 5]]]
 // 需返回结果数组
 
 function isArrary(x) {
-    return x.constructor === Array
+  return x.constructor === Array
 }
 
-function flatten(arr) {
-    if (!isArrary(arr)) return arr
-    const reuslt = []
-    for (let i = 0; i < arr.length; i++) {
-        const current = arr[i]
-
-        if (isArrary(current)) {
-            reuslt.push(...flatten(current))
-        } else {
-            reuslt.push(current)
-        }
+function flatten(arr = []) {
+  if (!Array.isArray(arr)) return arr
+  var res = []
+  for (var item of arr) {
+    if (Array.isArray(item)) {
+      res.push(...flatten(item))
+    } else {
+      res.push(item)
     }
-    return reuslt
+  }
+  return res
 }
 
 const r = flatten(list)
--- joy/structure/array/flattern/README.md
+++ joy/structure/array/flattern/README.md
@@ -1,1 +1,8 @@
 https://juejin.im/post/6844903779133816840
+
+https://www.zhihu.com/question/20507130
+
+recursive
+
+递归其实和循环是非常像的，循环都可以改写成递归，递归未必能改写成循环，这是一个充分不必要的条件。
+
