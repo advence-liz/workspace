a78ac28b6097937409618413c60c20805829d8c0
lizhuo13 <lizhuo13@meituan.com>
2020-11-24 21:00:22 +0800

    feat: add shake

---
+++ research/shake/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <h1>vue</h1>
+        hello world!!! shake
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
+
+<style></style>
---
+++ research/shake/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App.vue'
+const VConsole = require('vconsole/dist/vconsole.min')
+new VConsole()
+require('./shake')
+
+export default App
