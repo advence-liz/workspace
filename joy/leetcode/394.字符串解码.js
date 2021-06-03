22b3b8f9908330fc67d25461f2252f86dd6cac5a
advnece-liz <lizhuo13@meituan.com>
2021-06-03 13:29:47 +0800

    feat: 调整目录结构

---
+++ joy/leetcode/394.字符串解码.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=javascript
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ * 
+ * 3[2[bc]]
+ * 3  2
+ * bc
+ * 3[a]2[bc]
+ * 3 
+ * a 
+ * aaa
+ * 2
+ * bc
+ * aaa
+ */
+var decodeString = function(s) {
+
+    var stack = []
+
+    stack.push(s.charAt())
+
+};
+// @lc code=end
+
--- joy/structure/tree/deep/bintree-deep.js
+++
@@ -1,9 +0,0 @@
-function treeDeep(root) {
-    if (!root) return 0
-
-    return Math.max(treeDeep(root.left), treeDeep(root.right)) + 1
-}
-
-module.exports = {
-    treeDeep
-}
