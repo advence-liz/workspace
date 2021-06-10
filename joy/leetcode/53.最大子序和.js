33fefcda99383531c5ea375b1d26f7ad5186b081
advnece-liz <lizhuo13@meituan.com>
2021-06-10 13:38:25 +0800

    53.最大子序和

--- joy/leetcode/53.最大子序和.js
+++ joy/leetcode/53.最大子序和.js
@@ -11,7 +11,18 @@
  */
 var maxSubArray = function(nums) {
 
+    let max = nums[0] // max 赋 0 当数组中全部为负值就挂了 -Infinity
+    let sum = 0
+
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i]
+        max = Math.max(sum,max)
+        if(sum <0){
+            sum =0
+        }
+
+    }
+    return max
+
 };
 // @lc code=end
-
-https://m.dianping.com/awp/hfe/block/539884f43459/119826/index.html
