c8e8d33c835a46a0c8a8bec90d28a9819690c922
advnece-liz <lizhuo13@meituan.com>
2021-06-22 21:10:39 +0800

    feat: 添加qrcode

--- node/qrcode/sample.js
+++ node/qrcode/sample.js
@@ -1,6 +1,14 @@
-const QRCode = require('qrcode')
+// const QRCode = require('qrcode')
 
-QRCode.toString('http://www.google.com', function (err, string) {
-  if (err) throw err
-  console.log(string)
-})
+// QRCode.toString('http://www.google.com', function(err, string) {
+//   if (err) throw err
+//   console.log(string)
+// })
+var qrcode = require('qrcode-terminal');
+
+
+// qrcode.generate('http://github.com');
+
+
+qrcode.generate(`https://github.com/advence-liz/json-server-router`);
+
