e76daa29c3443a149bec5272355d31ce74822f70
advnece-liz <lizhuo13@meituan.com>
2021-07-05 11:31:26 +0800

    doc

---
+++ joy/leetcode/343.整数拆分.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=javascript
+ *
+ * [343] 整数拆分
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+
+    var dp = []
+    
+    dp[n] = dp[n]
+
+};
+// @lc code=end
+
---
+++ joy/leetcode/376.摆动序列.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=376 lang=javascript
+ *
+ * [376] 摆动序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function(nums) {
+
+};
+// @lc code=end
+
