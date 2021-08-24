36c58389ea80fda5f5189d6482c1f5f32ea15f39
advence-liz <advence_liz@163.com>
2021-08-24 22:53:07 +0800

    feat: 添加洗牌

--- joy/alg/数组/13-数组洗牌.js
+++ joy/alg/数组/13-数组洗牌.js
@@ -1,6 +1,17 @@
-function shuffle(arr){
+function shuffle(arr=[]){
 
-    let left =0;
-    let right =0;
+  let n = arr.length
+// Math.floor(Math.random()*n)  >> 0 ~ n-1
+  while(n!=0){
+      n--
+      let random = Math.floor(Math.random()*n)
+      let tmp = arr[n]
+      arr[n] = arr[random]
+      arr[random] = tmp
 
+  }
+  console.log(arr)
+  return arr
 }
+shuffle([1,2,3,4,5,6])
+
