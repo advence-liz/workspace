406e7c268fdc4a2473b0b5db4ed846a35ffe6094
advnece-liz <lizhuo13@meituan.com>
2021-09-16 12:49:33 +0800

    feat: 添加入口文件

---
+++ components/vue/slide-tab/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>vue</h1>
+    hello world!!!
+    <SlideTab />
+  </div>
+</template>
+
+<script>
+import SlideTab from './slide-tab'
+export default {
+  components: { SlideTab },
+  name: 'app'
+}
+</script>
+
+<style></style>
