cda8c634ec52a721044a594ef9e246d4febb3df9
lizhuo13 <lizhuo13@meituan.com>
2022-03-08 14:02:29 +0800

    feat: desc

--- joy/alg/链表/3-反转.js
+++ joy/alg/链表/3-反转.js
@@ -6,6 +6,8 @@ const { ListNode, createLinkList, print } = require('./helper')
 
 // 输入: 1->2->3->4->5->NULL
 // 输出: 5->4->3->2->1->NULL
+// pre cur
+// 1 <- 2 -> 3 ->4->5
 // 进阶:
 // 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
 
