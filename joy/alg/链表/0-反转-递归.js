ee4faa72b85eb6cdb3ac50326d23f7f4bc0f5e1e
lizhuo13 <lizhuo13@meituan.com>
2022-03-13 00:35:02 +0800

    feat: dsf

--- joy/alg/链表/0-反转-递归.js
+++ joy/alg/链表/0-反转-递归.js
@@ -7,7 +7,7 @@ const {ListNode,createLinkList,print} =require('./helper')
 
 // 输入: 1->2->3->4->5->NULL
 // 输出: 5->4->3->2->1->NULL
-// 进阶:
+// 进阶: 0<-0<-pre->cur->0->0
 // 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
 
 /**
@@ -15,14 +15,15 @@ const {ListNode,createLinkList,print} =require('./helper')
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    function reverse(pre,cur){
-      
+    function dsf(pre,cur){
         if(!cur) return pre
         let next = cur.next
         cur.next = pre
-        return reverse(cur,next)
+        dsf(cur,next)
+
+      
     }
-    return  reverse(null,head)
+    return  dsf(null,head)
 }
 
 var head = createLinkList([1,2,3,4,5])
