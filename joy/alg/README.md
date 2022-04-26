012a920ef74d325a1a91921dc15991106dcabe3c
lizhuo13 <lizhuo13@meituan.com>
2022-04-26 19:44:53 +0800

    upd

--- joy/alg/README.md
+++ joy/alg/README.md
@@ -1,3 +1,21 @@
 # 算法
 -[https://github.com/youngyangyang04/leetcode-master](https://github.com/youngyangyang04/leetcode-master)
 数组-> 链表-> 哈希表->字符串->栈与队列->树->回溯->贪心->动态规划->图论->高级数据结构
+
+
+## 二分
+
+35	Search Insert Position	★★	34	704	981					upper_bound													
+33	Search in Rotated Sorted Array	★★★	81	153	154	162	852			rotated / peak													
+69	Sqrt(x)	★★★								upper_bound													
+74	Search a 2D Matrix	★★★								treat 2d as 1d													
+875	Koko Eating Bananas	★★★	1011							guess ans and check													
+4	Median of Two Sorted Arrays	★★★★																					
+378	Kth Smallest Element in a Sorted Matrix	★★★★	668							kth + matrix													
+719	Find K-th Smallest Pair Distance	★★★★	786							kth + two pointers													
+
+## 前缀和
+https://zhuanlan.zhihu.com/p/344733609
+## TODO
+
+- [ ]719
--- research/animate-css/components/animation.vue
+++ research/animate-css/components/animation.vue
@@ -1,37 +1,40 @@
 <script>
 export default {
-  name: 'css-animation',
-  props: {
-    animation: { type: Array, default: [{ class: 'tx' }, { class: 'ty' }] },
-  },
-  render(createElement) {
-    const children = this.$slots.default
-    function createdNode(root, level) {
-      if (level === root.length) return children
-      const node = root[level]
-      return createElement(
-        'div',
-        {
-          class: node.class,
-          //   attrs: {
-          //     id: node,
-          //   },
+    name: 'css-animation',
+    props: {
+        animation: { type: Array, default: [{ class: 'tx' }, { class: 'ty' }] },
+    },
+    render(createElement) {
+        const children = this.$slots.default
+        function createdNode(root, level) {
+            if (level === root.length) return children
+            const node = root[level]
+            return createElement(
+                'div',
+                {
+                    class: node.class,
+                    //   attrs: {
+                    //     id: node,
+                    //   },
+                    on: {
+                        animationstart: this.animationstart,
+                    },
+                },
+                [createdNode(root, level + 1)]
+            )
+        }
+
+        const r = createdNode(this.animation, 0)
+
+        return r
+    },
+    methods: {
+        animationstart() {
+            console.log('animationstart', new Date())
         },
-        [createdNode(root, level + 1)]
-      )
-    }
-
-    const r = createdNode(this.animation, 0)
-
-    return r
-  },
-  methods: {
-    animationstart() {
-      console.log('animationstart', new Date())
+        animationend() {
+            console.log('animationend', new Date())
+        },
     },
-    animationend() {
-      console.log('animationend', new Date())
-    },
-  },
 }
 </script>
