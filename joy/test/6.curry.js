adc5b256519ca7e9ff2ef000c2bd26e854bc0d32
lizhuo13 <lizhuo13@meituan.com>
2023-05-31 23:47:13 +0800

    feat: upd

--- joy/test/6.curry.js
+++ joy/test/6.curry.js
@@ -1,21 +1,21 @@
-
 /**
  * curry(fn,...args)
  */
 
-function curry(fn,...args){
-
-    return (...rest)=>{
-        return  fn(...args,...rest)
+function curry(fn, ...args) {
+    return (...rest) => {
+        return fn(...args, ...rest)
     }
 }
 
-function add (x,y,z){
-    return x+y+z
+function add(x, y, z) {
+    return x + y + z
 }
 
-const curryAdd= curry(add,1)
+const curryAdd = curry(add, 1)
 
-const r =curryAdd(2,3)
+const curryAdd1 = curry(curryAdd, 2, 3)
+
+const r = curryAdd1(2, 3)
 
 console.log(r)
